d21106da0124a6ee66eb52915855db7c37fd80b5
ZahraRahmani2 <zahrarahmani@u.boisestate.edu>
2021-02-12 16:36:23 -0700

    order.css and order.php updated

--- menu.php
+++ menu.php
@@ -10,24 +10,24 @@
     </h1>
     <div class="content">
       <div class="menucolumn">
-        <img src="images/food1.PNG" alt="Food1"> <br>
+        <img src="images/food1.PNG" alt="Food1"> 
         <h2> Burger</h2>
-        <p><strong>$15</strong></p>
+        <p><strong>Price: $15</strong></p>
       </div>
       <div class="menucolumn">
         <img src="images/Chicken_biryani.PNG" alt="Food2"><br>
         <h2> Biryani</h2>
-        <p><strong> $25 </strong></p>
+        <p><strong>Price: $25 </strong></p>
       </div>
       <div class="menucolumn">
         <img src="images/Spagathti.PNG" alt="Food3"><br>
         <h2>Spaghetti</h2>
-        <p><strong>$20 </strong></p>
+        <p><strong>Price: $20 </strong></p>
       </div>
       <div class="menucolumn">
         <img src="images/food2.PNG" alt="Food4"><br>
         <h2> Falafel</h2>
-        <p><strong> $10 </strong></p>
+        <p><strong>Price: $10 </strong></p>
       </div>
       <button onclick="gohome()">Homepage</button>
       <script>
---
+++ order.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="header.css"> 
+    <link rel="stylesheet" href="order.css">
+  </head>
+  <body>
+    <?php include "header.php"; ?>
+	<h1 id="foodOrderText">
+	Food Orders 
+	</h1>
+    <div class="order-content">
+		<div class="order-details">
+			<img src="images/Chicken_biryani.PNG" alt="Food Picture">
+	        <h2> Burger</h2>
+	        <p><strong>Price: $15</strong></p>
+		</div>
+
+		<button>Add to Cart</button>
+	</div>
+    <?php include "footer.php"; ?>
+  </body>
+</html>
