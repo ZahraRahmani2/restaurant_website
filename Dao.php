819c06223bb9eeb64a019314e1f65aa64d4a65b0
ZahraRahmani2 <zahrarahmani@u.boisestate.edu>
2021-03-12 08:22:15 -0700

    commit 3 hero

--- Dao.php
+++ Dao.php
@@ -1,41 +1,61 @@
-<?php
 
-class Dao {
-  public $dsn = 'mysql:dbname=zrahmani;host=127.0.0.1';
-  public $user = 'zrahmani';
-  public $password = 'My_personal_db5548';
-  //public $port = "3308";
+<?php 
+//Get Heroku ClearDB connection information
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL
+"));
+$cleardb_server   = $cleardb_url["127.0.0.1"];
+$cleardb_username = $cleardb_url["zrahmani"];
+$cleardb_password = $cleardb_url["My_personal_db5548"];
+$cleardb_db       = substr($cleardb_url["zrahmani"],1);
 
-  private function getConnection () {
-    try {
-      $connection = new PDO($this->dsn, $this->user, $this->password);
-    } catch (PDOException $e) {
-        echo 'Connection failed: ' . $e->getMessage();
-    }
-    return $connection;
-  }
-
-  public function insertUser ($username, $lastName, $password, $email, $phoneNumber) {
-    $conn = $this->getConnection();
-    $saveQuery = "insert into users (username, lastName, password, email, phoneNumber)
-     values (:username, :lastName, :password, :email, :phoneNumber)";
-    $q = $conn->prepare($saveQuery);
-    $q->bindParam(":username", $username);
-    $q->bindParam(":lastName", $lastName);
-    $q->bindParam(":password", $password);
-    $q->bindParam(":email", $email);
-    $q->bindParam(":phoneNumber", $phoneNumber);
-    $q->execute();
-  }
-
-  public function getfoods () {
-    $connection = $this->getConnection();
-    try {
-      $rows = $connection->query("select * from foods", PDO::FETCH_ASSOC);
-    } catch(Exception $e) {
-      echo print_r($e,1);
-      exit;
-    }
-    return $rows;
-  }
+try {
+    $pdo = new PDO("mysql:host=".$cleardb_server."; dbname=".$cleardb_db, $cleardb_username, $cleardb_password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "it worked";
+    $pdo = null;
+} catch (PDOException $e) {
+    echo "Error!: " . $e->getMessage() . "<br/>";
+    die();
 }
+
+
+
+// class Dao {
+//   public $dsn = 'mysql:dbname=zrahmani;host=127.0.0.1';
+//   public $user = 'zrahmani';
+//   public $password = 'My_personal_db5548';
+//   //public $port = "3308";
+
+//   private function getConnection () {
+//     try {
+//       $connection = new PDO($this->dsn, $this->user, $this->password);
+//     } catch (PDOException $e) {
+//         echo 'Connection failed: ' . $e->getMessage();
+//     }
+//     return $connection;
+//   }
+
+//   public function insertUser ($username, $lastName, $password, $email, $phoneNumber) {
+//     $conn = $this->getConnection();
+//     $saveQuery = "insert into users (username, lastName, password, email, phoneNumber)
+//      values (:username, :lastName, :password, :email, :phoneNumber)";
+//     $q = $conn->prepare($saveQuery);
+//     $q->bindParam(":username", $username);
+//     $q->bindParam(":lastName", $lastName);
+//     $q->bindParam(":password", $password);
+//     $q->bindParam(":email", $email);
+//     $q->bindParam(":phoneNumber", $phoneNumber);
+//     $q->execute();
+//   }
+
+//   public function getfoods () {
+//     $connection = $this->getConnection();
+//     try {
+//       $rows = $connection->query("select * from foods", PDO::FETCH_ASSOC);
+//     } catch(Exception $e) {
+//       echo print_r($e,1);
+//       exit;
+//     }
+//     return $rows;
+//   }
+// }
