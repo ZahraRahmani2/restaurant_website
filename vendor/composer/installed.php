e7fdbda7bedda5ba12f17ea7ad72efed4d98a762
ZahraRahmani2 <zahrarahmani@u.boisestate.edu>
2021-03-12 11:46:59 -0700

    commit 9 heroku

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f238a06a625e8d21825bd2cda6c8b4622adc5c26',
+    'name' => 'pacific/sierra75275',
+  ),
+  'versions' => 
+  array (
+    'pacific/sierra75275' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f238a06a625e8d21825bd2cda6c8b4622adc5c26',
+    ),
+  ),
+);
