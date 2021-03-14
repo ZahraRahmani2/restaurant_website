161cd64d0bd492ffb4d0f126c5f1df1d94f555c9
ZahraRahmani2 <zahrarahmani@u.boisestate.edu>
2021-03-13 23:17:33 -0700

    details_info changed

--- detail_information_handler.php
+++ detail_information_handler.php
@@ -1,15 +1,19 @@
 <?php
-  $message = "Your order was recieved";
   echo "
     <script type='text/javascript'>
         document.getElementById('fname').value='';
         document.getElementById('lname').value='';
         document.getElementById('email').value='';
         document.getElementById('phone_number').value='';
-        await sleep(5000);
-        alert('$message');
-        await sleep(5000);
+        
+        function sleep (time) {
+            return new Promise((resolve) => setTimeout(resolve, time));
+        }
+        sleep(500).then(() => {
+            alert('Your order was recieved');
+        });
     </script>";
 
   header('Location: details_Information.php');
+  
   exit;
