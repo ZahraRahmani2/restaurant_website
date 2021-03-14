942d439e34f616044ba4c492b8d85516d8605f87
ZahraRahmani2 <zahrarahmani@u.boisestate.edu>
2021-03-14 00:03:09 -0700

    Almost done

--- detail_information_handler.php
+++ detail_information_handler.php
@@ -1,5 +1,6 @@
 <?php
-  header('Location: details_Information.php');
   echo "<h3>". $_GET['fname'].$_GET['lname']."we have recieved your order</h3>";
+  header('Location: details_Information.php');
   
+
   exit;
