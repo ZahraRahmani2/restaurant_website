efa32d682c497984804d788ac1e2573526becd23
ZahraRahmani2 <zahrarahmani@u.boisestate.edu>
2021-03-13 17:04:01 -0700

    pdo connection check

--- Dao.php
+++ Dao.php
@@ -1,63 +1,76 @@
 
 <?php 
-class Dao {
-  public $dsn = 'mysql:dbname=heroku_7876bf830b7489b;host=us-cdbr-east-03.cleardb.com';
-  public $user = 'b53b15900c6ae4';
-  public $password = '4b376ff1';
-  public $port = "3306";
+  $dsn = 'mysql:dbname=heroku_3755c6c9c8a755b;host=us-cdbr-east-03.cleardb.com';
+  $user = 'b82417261244e5';
+  $password = '06d87278';
+  $port = "3306";
 
-private function getConnection () {
     try {
-    $connection = new PDO($this->dsn.":".$this->port, $this->user, $this->password);
+    $connection = new PDO($dsn.":".$port, $user, $password);
         echo "It worked";
     } catch (PDOException $e) {
         echo 'Connection failed: ' . $e->getMessage();
     }
-    return $connection;
-}
-public function insertUser ($username, $lastName, $password, $email, $phoneNumber) {
-    $conn = $this->getConnection();
-    $saveQuery = "insert into users (username, lastName, password, email, phoneNumber)
-     values (:username, :lastName, :password, :email, :phoneNumber)";
-    $q = $conn->prepare($saveQuery);
-    $q->bindParam(":username", $username);
-    $q->bindParam(":lastName", $lastName);
-    $q->bindParam(":password", $password);
-    $q->bindParam(":email", $email);
-    $q->bindParam(":phoneNumber", $phoneNumber);
-    $q->execute();
-}
 
-public function getfoods () {
-    $connection = $this->getConnection();
-    try {
-      $rows = $connection->query("select * from foods", PDO::FETCH_ASSOC);
-    } catch(Exception $e) {
-      echo print_r($e,1);
-      exit;
-    }
-    return $rows;
-  }
+// class Dao {
+//   public $dsn = 'mysql:dbname=heroku_3755c6c9c8a755b;host=us-cdbr-east-03.cleardb.com';
+//   public $user = 'b82417261244e5';
+//   public $password = '06d87278';
+//   public $port = "3306";
 
-  public function addToCart ($id) {
-    $conn = $this->getConnection();
-    $saveQuery = "insert into shoppingcart (foodID)
-     values (:id)";
-    $q = $conn->prepare($saveQuery);
-    $q->bindParam(":id", $id);
-    $q->execute();
-  }
+// private function getConnection () {
+//     try {
+//       $connection = new PDO($this->dsn.":".$this->port, $this->user, $this->password);
+//         echo "It worked";
+//     } catch (PDOException $e) {
+//         echo 'Connection failed: ' . $e->getMessage();
+//     }
+//     return $connection;
+// }
 
-  public function getCards () {
-    $connection = $this->getConnection();
-    try {
-      $rows = $connection->query("select * from shoppingcart join foods sc on sc.foodID=foods.foodID", PDO::FETCH_ASSOC);
-    } catch(Exception $e) {
-      echo print_r($e,1);
-      exit;
-    }
-    return $rows;
-  }
+// public function insertUser ($username, $lastName, $password, $email, $phoneNumber) {
+//     $conn = $this->getConnection();
+//     $saveQuery = "insert into users (username, lastname, passwrd, email, phoneNumber)
+//      values (:username, :lastName, :password, :email, :phoneNumber)";
+//     $q = $conn->prepare($saveQuery);
+//     $q->bindParam(":username", $username);
+//     $q->bindParam(":lastName", $lastName);
+//     $q->bindParam(":password", $password);
+//     $q->bindParam(":email", $email);
+//     $q->bindParam(":phoneNumber", $phoneNumber);
+//     $q->execute();
+// }
+
+// public function getfoods () {
+//     $connection = $this->getConnection();
+//     try {
+//       $rows = $connection->query("select * from foods", PDO::FETCH_ASSOC);
+//     } catch(Exception $e) {
+//       echo print_r($e,1);
+//       exit;
+//     }
+//     return $rows;
+//   }
+
+//   public function addToCart ($id) {
+//     $conn = $this->getConnection();
+//     $saveQuery = "insert into shoppingcart (foodID)
+//      values (:id)";
+//     $q = $conn->prepare($saveQuery);
+//     $q->bindParam(":id", $id);
+//     $q->execute();
+//   }
+
+//   public function getCards () {
+//     $connection = $this->getConnection();
+//     try {
+//       $rows = $connection->query("select * from shoppingcart join foods sc on sc.foodID=foods.foodID", PDO::FETCH_ASSOC);
+//     } catch(Exception $e) {
+//       echo print_r($e,1);
+//       exit;
+//     }
+//     return $rows;
+//   }
   
 
-}
+// }
